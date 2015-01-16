538186c9ebb0b91837222c57264b0dfeab7badfa
Joffrey BION <joffrey.bion@gmail.com>
2015-01-16 22:16:33 +0100

    improved logging API

--- log/logging.go
+++ log/logging.go
@@ -7,29 +7,63 @@ import (
 )
 
 var (
-	logger *log.Logger = log.New(os.Stdout, "bot: ", 0)
+	logger *log.Logger = log.New(os.Stdout, "bot: ", log.Lshortfile)
 )
 
-func E(args ...interface{}) {
-	logger.Println(args)
+const (
+	lvlError int = iota
+	lvlWarning
+	lvlInfo
+	lvlDebug
+	lvlVerbose
+)
+
+const (
+	LEVEL int = lvlVerbose
+)
+
+func printLog(level int, args ...interface{}) {
+	if level <= LEVEL {
+		logger.Println(args...)
+	}
 }
 
-func Ef(formatStr string, args ...interface{}) {
-	E(fmt.Sprintf(formatStr, args))
+func E(args ...interface{}) {
+	printLog(lvlError, args...)
 }
 
 func W(args ...interface{}) {
-	logger.Println(args)
+	printLog(lvlWarning, args...)
 }
 
 func I(args ...interface{}) {
-	logger.Println(args)
+	printLog(lvlInfo, args...)
 }
 
 func D(args ...interface{}) {
-	logger.Println(args)
+	printLog(lvlDebug, args...)
 }
 
 func V(args ...interface{}) {
-	logger.Println(args)
+	printLog(lvlVerbose, args...)
+}
+
+func Ef(formatStr string, args ...interface{}) {
+	E(fmt.Sprintf(formatStr, args...))
+}
+
+func Wf(formatStr string, args ...interface{}) {
+	W(fmt.Sprintf(formatStr, args...))
+}
+
+func If(formatStr string, args ...interface{}) {
+	I(fmt.Sprintf(formatStr, args...))
+}
+
+func Df(formatStr string, args ...interface{}) {
+	D(fmt.Sprintf(formatStr, args...))
+}
+
+func Vf(formatStr string, args ...interface{}) {
+	V(fmt.Sprintf(formatStr, args...))
 }
